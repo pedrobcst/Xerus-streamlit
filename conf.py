1f1d2c38f8b109e33f5da51ec07c0e99732dd1e4
pedrobcst <pedrobcst@gmail.com>
2022-03-24 18:50:03 +0900

    Added a better dataframe

--- conf.py
+++ conf.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 
 @dataclass
 class AppSettings:
     TMP_FOLDER: str = "tmp"
     RESULTS_TMP_FOLDER: str = "results_tmp"
+    DROP_COLUMNS: Tuple = (
+        "filename",
+        "spacegroup_number",
+        "full_path",
+        "nruns",
+        "pos",
+        "gpx_path"
+    )
+
